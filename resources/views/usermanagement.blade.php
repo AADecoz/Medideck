bce3e5863632580ed8f5ac6164313c736faeb50f
AADecoz <82182504+AADecoz@users.noreply.github.com>
2021-09-03 16:52:04 +0200

    Table improvements UserManagement

--- resources/views/usermanagement.blade.php
+++ resources/views/usermanagement.blade.php
@@ -1,3 +1,4 @@
+<link href="{{ asset('css/table.css') }}" rel="stylesheet">
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -10,15 +11,30 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="table-fixed">
+                        <tr>
+                            <th>ID</th>
+                            <th>User</th>
+                            <th>Email</th>
+                            <th>Subscriptions</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @if ($user->role_id = 3)
                     @foreach($user as $users)
-                        <thead>
                             <tr>
+                            <td>{{$users->id}}</td>
                             <td>{{$users->name}}</td>
-                            <td>{{$users->email}}</td>
-{{--                            <td>{{$users->}}</td>--}}
+                            <td><a href="mailto:{{$users->email}}">{{$users->email}}</a></td>
+                            <td></td>
+                                <td><x-button class="ml-3" style="background: rgb(0,120,98)">
+                                    {{ __('Edit') }}
+                                </x-button></td>
+                                <td><x-button class="ml-3" style="background: rgb(9,40,105)">
+                                        {{ __('Delete') }}
+                                </x-button></td>
                             </tr>
-                        </thead>
                     @endforeach
+                            @endif
                     </table>
                 </div>
             </div>
